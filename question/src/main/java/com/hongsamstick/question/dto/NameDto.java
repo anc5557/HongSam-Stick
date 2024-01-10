5be4b210145e94664da59d1f6eae00abcb223615
창공 <anc5557@naver.com>
2024-01-10 13:04:58 +0900

    fix: Email, NameDto 기본생성자 추가

--- question/src/main/java/com/hongsamstick/question/dto/NameDto.java
+++ question/src/main/java/com/hongsamstick/question/dto/NameDto.java
@@ -1,12 +1,12 @@
 package com.hongsamstick.question.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@NoArgsConstructor
 public class NameDto {
 
   private String name;
