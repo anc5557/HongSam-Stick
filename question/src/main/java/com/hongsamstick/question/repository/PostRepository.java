b0f6aa330b23f888cf93222bfe5bd71a2fa6f830
창공 <anc5557@naver.com>
2024-03-02 18:36:25 +0900

    코드 변경 내용 추가***
    ***개설한 게시판 목록 보여주기*** 메서드 추가
    ***code로 게시글 찾기*** 메서드 추가
    ***내가 쓴 게시글 찾기*** 메서드 추가
    ***코드로 게시물 찾기*** 페이지 추가
    ***코드로 게시물 찾기*** 기능 추가

--- question/src/main/java/com/hongsamstick/question/repository/PostRepository.java
+++ question/src/main/java/com/hongsamstick/question/repository/PostRepository.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
   // code로 게시글 찾기
@@ -24,4 +25,17 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     LocalDateTime currentDate,
     Pageable pageable
   );
+
+  // 내가 쓴 게시글 찾기 (페이지네이션 적용)
+  @Query(
+    "SELECT p FROM Post p WHERE p.member.email = :email AND (p.endDate > CURRENT_TIMESTAMP OR p.endDate IS NULL OR :excludeEnded = false)"
+  )
+  Page<Post> findByMemberEmailAndExcludeEnded(
+    String email,
+    boolean excludeEnded,
+    Pageable pageable
+  );
+
+  // 코드로 게시글 찾기(마감 기한이 지나지 않은 게시글)
+  Optional<Post> findByCodeAndEndDateAfter(UUID code, LocalDateTime currentDate);
 }
